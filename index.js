65ba954701d7020bc944f46b59c3b34cf40d4a00
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2023-05-06 17:09:18 +0100

    Added config :sparkles:

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks", "import-helpers"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [
+          ["/^react/", "/^next/"],
+          "module",
+          "/^@//",
+          ["parent", "sibling", "index"],
+        ],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
+  },
+};
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Eslint Config React
+
+A simple eslint config for react projects.
+
+## Installation
+
+```bash
+npm install --save-dev @gui-marc/eslint-config-react
+```
+
+## Usage
+
+Add the following to your `.eslintrc` file:
+
+```json
+{
+  "extends": "@gui-marc/eslint-config-react"
+}
+```
+
+And this to your `prettierrc` file:
+
+```json
+{
+  "singleQuote": true,
+  "trailingComma": "es5"
+}
+```
